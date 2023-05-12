b867d8bfe3899716913358b7317b90a1be6f1029
amaiaetxarte <132052285+amaiaetxarte@users.noreply.github.com>
2023-05-12 10:52:29 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 255)
+basic.pause(100)
+mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Back, 255)
+basic.pause(100)
+mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Left, 255)
+basic.pause(100)
+mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Right, 255)
+basic.pause(100)
